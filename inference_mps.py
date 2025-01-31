948be10b1e197fe39f705bc3aad76e2454fa3c07
Hyunsung Kim <hyunsung.kim@kaist.ac.kr>
2025-01-31 23:35:35 +0900

    ✅ Update inference_mps.py and test.ipynb

--- inference_mps.py
+++ inference_mps.py
@@ -25,10 +25,23 @@ def get_drop_frames(n_frames: int) -> Tuple[int, np.ndarray]:
 if __name__ == "__main__":
     model_name = "yolox_s"
     exp_path = "exps/example/custom/yolox_s.py"
-    ckpt_path = "YOLOX_outputs/yolox_s/best_ckpt.pth"
-    video_path = "datasets/sample_match.mp4"
-    device = torch.device("mps") if torch.backends.mps.is_available() else "cpu"
-    bbox_cols = ["x1", "y1", "x2", "y2", "conf1", "conf2", "class"]
+    ckpt_path = f"YOLOX_outputs/{model_name}/best_ckpt.pth"
+
+    video_name = "GX010224_Trim_18.07"
+    video_path = f"datasets/soccer/videos/{video_name}.mp4"
+    bbox_path = f"YOLOX_outputs/{model_name}/bboxes/{video_name}.csv"
+
+    max_frame = 3000
+    step_size = 30
+    min_conf = 0.1
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = "cpu"
+    print(f"Device: {device}")
 
     exp = get_exp(exp_path, model_name)
     ckpt = torch.load(ckpt_path, weights_only=False, map_location="cpu")
@@ -46,9 +59,9 @@ if __name__ == "__main__":
     bbox_cols = ["x1", "y1", "x2", "y2", "conf1", "conf2", "class"]
     bbox_list = []
 
-    for i in tqdm(np.setdiff1d(np.arange(1, n_frames), drop_frames)[:3000]):
+    for i in tqdm(np.setdiff1d(np.arange(1, n_frames), drop_frames)[:max_frame]):
         video.grab()
-        if i % 30 != 0:
+        if i % step_size != 0:
             continue
 
         ret, image = video.retrieve()
@@ -72,10 +85,10 @@ if __name__ == "__main__":
 
     bboxes = pd.concat(bbox_list, ignore_index=True)
     bboxes[bbox_cols[:4]] = bboxes[bbox_cols[:4]].round(2)
-    bboxes["conf"] = bboxes["conf1"] * bboxes["conf2"]
+    bboxes["confidence"] = bboxes["conf1"] * bboxes["conf2"]
     bboxes["class"] = bboxes["class"].map({0: "player", 32: "ball"})
-    bboxes = bboxes[["frame"] + bbox_cols[:4] + ["conf", "class"]]
+    bboxes = bboxes.loc[bboxes["confidence"] > min_conf, ["frame"] + bbox_cols[:4] + ["confidence", "class"]]
     print(bboxes)
 
     os.makedirs("YOLOX_outputs/yolox_s/bboxes", exist_ok=True)
-    bboxes.to_csv("YOLOX_outputs/yolox_s/bboxes/sample_video_mps.csv")
+    bboxes.to_csv(bbox_path, index=False)
